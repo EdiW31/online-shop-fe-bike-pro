585d4a4eb296dd1efe0d8aee37a57b160f794f8c
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-07-03 12:55:35 +0300

    Initial Commit + listen to BE url

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+interface User {
+  id: string;
+  email: string;
+}
+
 
 function App() {
+
+  const [user, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch(`${backendUrl}/users`).then((response) => {
+      response.json().then((data) => {
+        setUsers(data);
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users</h1>
+      <ul>
+      {user.map((user) => (
+        <li key={user.id}>
+          <div>{user.email}</div>
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
-
+// - Project structure:
+//   - src/
+//     - components/ (Reusable UI components)
+//       - Navbar/
+//         - Navbar.tsx
+//         - navbar.css
+//       - Footer/
+//         - Footer.tsx
+//         - footer.css
+//       - ProductCard/
+//         - ProductCard.tsx
+//         - productCard.css
+//     - pages/ (Different pages of the application)
+//       - Home/
+//         - Home.tsx
+//         - home.css
+//       - ProductList/
+//         - ProductList.tsx
+//         - productList.css
+//       - ProductDetails/
+//         - ProductDetails.tsx
+//         - productDetails.css
+//       - Cart/
+//         - Cart.tsx
+//         - cart.css
+//       - Checkout/
+//         - Checkout.tsx
+//         - checkout.css
 export default App;
